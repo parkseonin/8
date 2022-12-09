43856204846ca1cb67b2eda42db871d0ecb75fa0
Heejin Moom <shahee200234@naver.com>
2022-12-09 15:34:23 +0900

    test

--- DOI.py
+++ DOI.py
@@ -1,19 +1,16 @@
 import cv2
 
-imgfile = './image/para.jpg'
-cascade_file = "haarcascade_frontalface_default.xml"
+image = cv2.imread('./image/para.jpg')
+eye_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-image = cv2.imread(imgfile)
-cascade = cv2.CascadeClassifier(cascade_file)
-
-face_list = cascade.detectMultiScale(image, scaleFactor=1.14, minNeighbors=2, minSize=(50, 50), maxSize = (60, 60))
+face_list = eye_cascade.detectMultiScale(image, scaleFactor=1.11, minNeighbors=4, minSize=(50, 50), maxSize = (60, 60))
 
 if len(face_list) > 0:    
-  color = (0, 247, 230)
+  color = (0, 0, 0)
   for face in face_list:
     x, y, w, h = face
     # 사각형 그리기
-    cv2.rectangle(image,(x,y+int(h/28*10)),(x+w,y+int(h/28*14)),color,-5)
+    cv2.rectangle(image,(int(x-10),int(y-5)),(int(x+10)+w,y+int(h/28*14)),color,-5)
 
 else:
   print("none") 
